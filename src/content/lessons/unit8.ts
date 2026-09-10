f1067b5bae712c448f89121dfcec75351efd0209
anannt-education <hello.anannt@gmail.com>
2026-09-10 13:13:26 +0530

    Wire unit 8 lessons from split modules.

--- src/content/lessons/unit8.ts
+++ src/content/lessons/unit8.ts
@@ -1,1 +1,4 @@
-PLACEHOLDER_TOO_LARGE_USE_JSON
+import { UNIT8_LESSONS_A } from "./unit8-a";
+import { UNIT8_LESSONS_B } from "./unit8-b";
+
+export const UNIT8_LESSONS = [...UNIT8_LESSONS_A, ...UNIT8_LESSONS_B];
